2cf26b1a6893b73e4d049e7b90a0ab302ac9ac53
Marcus <marcus.h.lancaster@gmail.com>
2018-05-25 11:49:17 +0100

    Added docstrings to jsonify.py

--- jsonify.py
+++ jsonify.py
@@ -7,17 +7,40 @@ def main():
     saveAsJSON("logdata.json", d)
 
 def logfileParser(files):
-    data = {}
-    for filepath in files:
-        with open(filepath, 'r') as f:
-            for line in f:
-                user,logdata = lineParser(line)
-                if user not in data:
-                    data[user] = []
-                data[user].append(logdata)
-    return data
+    """
+    Transforms log data contained in multiple files into a python
+    dictionary format in the format:
+    {
+        "user id0":[{timestamp0, input0},...,{timestampN,inputN}],
+        ...
+        "user idN":[{timestamp0, input0},...,{timestampN,inputN}]
+    }
+    
+    Keyword Arguments:
+    files -- A single file name, or a list of files.
+    """
+    if type(files) is not list:
+        return logfileParser([files])
+    else:
+        data = {}
+        for filepath in files:
+            with open(filepath, 'r') as f:
+                for line in f:
+                    user,logdata = lineParser(line)
+                    if user not in data:
+                        data[user] = []
+                    data[user].append(logdata)
+        return data
 
 def lineParser(line):
+    """
+    Converts individual lines of log files into constituent components.
+    Returns the user ID and an opject containing the timestamp and the 
+    code written.
+
+    Keyword Arguments:
+    line -- String containing a single line from the log file
+    """
     try:
         userDate,code = line.split(" > ", 1)
         date,time,tz,user = userDate.split(" ")
@@ -29,6 +52,13 @@ def lineParser(line):
     return user,{"timestamp":datetimeString,"input":code.strip()}
                 
 def saveAsJSON(filename, values):
+    """
+    Creates a file in JSON format from python dictionary object.
+
+    Keyword Arguments:
+    filename -- String containing the name of the file (includes filetype)
+    values -- Dictionary object containing data to be written to file
+    """
     with open(filename, "w") as output:
         output.write(
                 json.dumps(
