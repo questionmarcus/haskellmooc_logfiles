e8e7e6c6caf9ba2a35a644d09dc23bc736017d7e
Marcus <marcus.h.lancaster@gmail.com>
2018-08-25 19:28:28 +0200

    added test data and unittests

---
+++ test_jsonify.py
@@ -0,0 +1,13 @@
+import unittest
+from jsonify import *
+
+class JsonifyTestSuite(unittest.TestCase):
+
+    def testWebParse(self):
+        pass
+
+    def testLineParser(self):
+        pass
+
+    def testUserSessions(self):
+        pass
